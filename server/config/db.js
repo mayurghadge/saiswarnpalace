738446757fa0379ef0b841b1d42dcd840e285589
mayur ghadge <ghadgemayur727@gmail.com>
2026-07-16 16:54:08 +0530

    Added API root route

--- server/config/db.js
+++ server/config/db.js
@@ -5,6 +5,7 @@ console.log("========== DB CONFIG ==========");
 console.log("DB_SERVER =", process.env.DB_SERVER);
 console.log("DB_NAME =", process.env.DB_NAME);
 console.log("DB_USER =", process.env.DB_USER);
+console.log("DB_PASSWORD =", process.env.DB_PASSWORD);
 console.log("DB_PORT =", process.env.DB_PORT);
 console.log("===============================");
 
