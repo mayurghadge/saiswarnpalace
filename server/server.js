7f0386a4a71f861e25c815b11f4ddb671e746521
mayur ghadge <ghadgemayur727@gmail.com>
2026-07-16 22:34:36 +0530

    Added API root route

--- server/server.js
+++ server/server.js
@@ -4,14 +4,6 @@ const dotenv = require('dotenv');
 const path = require('path');
 dotenv.config();
 
-console.log("========== ENV CHECK ==========");
-console.log("DB_SERVER =", process.env.DB_SERVER);
-console.log("DB_NAME =", process.env.DB_NAME);
-console.log("DB_USER =", process.env.DB_USER);
-console.log("DB_PORT =", process.env.DB_PORT);
-console.log("DB_PASSWORD =", process.env.DB_PASSWORD);
-console.log("===============================");
-
 const cloudinary = require('./config/cloudinary');
 const upload = require('./config/upload');
 const jwt = require('jsonwebtoken');
