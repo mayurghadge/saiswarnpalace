aa2f78c0d0831270f0723d77a743a255b5055768
mayur ghadge <ghadgemayur727@gmail.com>
2026-08-05 19:08:48 +0530

    Added API root route

--- server/tmp_refresh_debug.js
+++ server/tmp_refresh_debug.js
@@ -46,6 +46,13 @@ function mockDbModule(mockExports) {
 
   mockDbModule({ connectDB: async () => mockPool, sql: {} });
   process.env.NODE_ENV = 'test';
+  process.env.FRONTEND_URL = 'http://localhost:5173';
+  process.env.ACCESS_TOKEN_EXPIRES = '15m';
+  process.env.REFRESH_TOKEN_DAYS = '30';
+  process.env.REFRESH_TOKEN_BINDING_STRICT = 'false';
+  process.env.SENTRY_DSN = '';
+  process.env.DB_DRIVER = 'tedious';
+  process.env.JWT_SECRET = 'test-secret';
   const app = require('./server');
   const request = supertest(app);
 
